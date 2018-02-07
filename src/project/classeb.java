f137b491b7f192deb56d32cc9d741411f96a7c0a
achrefbenjazia <mohamedachref.benjazia@esprit.tn>
2018-02-07 00:24:52 -1200

    eee

--- src/project/classeb.java
+++ src/project/classeb.java
@@ -9,13 +9,6 @@ package project;
  *
  * @author asus
  */
-public class Project {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class classeb {
     
 }
